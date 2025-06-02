a4a09c9c4f85c12f92bd7279dc46b76bc98b98af
dustorb <k.alina.03a@gmail.com>
2025-06-02 03:11:47 +0300

    main char

--- Assets/Scripts/Old/Moving/PlayerMove.cs
+++ Assets/Scripts/Old/Moving/PlayerMove.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     public float speed = 5.0f;
+    public Animator animator;
 
     void Update()
     {
@@ -13,5 +14,14 @@ public class PlayerMove : MonoBehaviour
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
         transform.Translate(movement * speed * Time.deltaTime, Space.World);
+
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0 )
+        {
+            animator.Play("Walk");
+        }
+        else
+        {
+            animator.Play("Idle");
+        }
     }
 }
